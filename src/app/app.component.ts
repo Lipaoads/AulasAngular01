54e1abe6b052ff185cf36679d52912a44ec97b2b
Lipaoads <107074065+Lipaoads@users.noreply.github.com>
2023-05-31 04:26:14 -0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Pessoa } from './pessoa';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  
+})
+export class AppComponent {
+  title = 'AulasAngular01';
+  nomes = [
+    new Pessoa(1, 'Paulo,', 40),
+    new Pessoa(2, 'Adriele,', 34),
+    new Pessoa(3, 'tonhe,', 64),
+    new Pessoa(4, 'Raimundo,', 65),
+    
+];
+
+
+nomePrincipal = this.nomes[0];
+  
+}
